03c14d561da1100c0c5752b4aaa9e96809aec68c
adamjgoodman <goodmanadamj@gmail.com>
2020-03-10 20:05:30 -0400

    devise, add usernames

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+         :validatable, authentication_keys: [:login]
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+end
