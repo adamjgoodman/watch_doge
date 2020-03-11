cb260a976bdb95b635918b0b8aa52c2c47d611b0
adamjgoodman <goodmanadamj@gmail.com>
2020-03-11 00:11:32 -0400

    new article, beginning index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,27 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @articles = Article.all 
   end
+
+  def new
+    @article = Article.new
+  end
+
+	def create
+		@article = current_user.articles.create(article_params)
+		if @article.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
+  end
+  
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :category, :content)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_many :articles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
-         :validatable, authentication_keys: [:login]
+         :validatable, authentication_keys: [:email]
   attr_writer :login
 
   def login
